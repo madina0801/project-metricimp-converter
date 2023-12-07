b1313b6939af37d94363a7c32dd4dc27f762601e
Madina <madinatussupova0820@gmail.com>
2023-12-07 14:01:04 +0600

    convertHandler and route implemented

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,114 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
+  this.getNum = function (input) {
+    let result = input.match(/\d+\.\d+/)[0];
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.getUnit = function (input) {
+    let result = input.match(/[a-zA-Z]+/)[0].toLowerCase();
+
+    if (result.length > 3) {
+      return false;
+    }
+
+    switch (result) {
+      case 'km':
+        return 'km'
+      case 'gal':
+        return 'gal'
+      case 'lbs':
+        return 'lbs'
+      case 'mi':
+        return 'mi'
+      case 'l':
+        return 'L';
+      case 'kg':
+        return 'kg'
+      default:
+        return undefined;
+    }
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    let unit = initUnit.toLowerCase();
+    switch (unit) {
+      case 'kg':
+        return 'lbs'
+      case 'l':
+        return 'gal'
+      case 'mi':
+        return 'km'
+      case 'lbs':
+        return 'kg'
+      case 'gal':
+        return 'L'
+      case 'km':
+        return 'mi'
+      default:
+        return undefined
+    }
+  };
+
+  this.spellOutUnit = function (unit) {
+    unit = unit.toLowerCase();
+    switch (unit) {
+      case 'km':
+        return 'kilometers'
+      case 'gal':
+        return 'gallons'
+      case 'lbs':
+        return 'pounds'
+      case 'mi':
+        return 'miles'
+      case 'l':
+        return 'liters'
+      case 'kg':
+        return 'kilograms'
+      default:
+        return undefined
+    }
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+    let unit = initUnit.toLowerCase();
     let result;
     
+    switch(unit){
+      case 'km':
+        result = initNum / miToKm
+        break
+      case 'mi':
+        result = initNum * miToKm
+        break
+      case 'kg':
+        result = initNum / lbsToKg
+        break
+      case 'lbs':
+        result = initNum * lbsToKg
+        break
+      case 'l':
+        result = initNum / galToL
+        break
+      case 'gal':
+        result = initNum * galToL
+        break
+      default:
+        result = undefined
+    }
+    return parseFloat(result.toFixed(5));
+  };
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    let result = `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
+
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,24 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
+  app.route('/api/convert').get((req, res) => {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
 
-};
+    if (!initNum && !initUnit) {
+      res.send("Invalid Input!")
+    } else if (!initNum) {
+      res.send("Invalid Number!")
+    } else if (!initUnit) {
+      res.send("Invalid Unit!")
+    }
+
+    let returNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let returnString = convertHandler.getString(initNum, initUnit, returNum, returnUnit);
+
+    res.json({ initNum, initUnit, returNum, returnUnit, string: returnString })
+  })
+}
